e33b467f2fea712d5e0795d6ba31b22284131dd3
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-22 19:04:16 +0530

    it is udemy exercise.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      PlayingCat.isCatPlaying(true,30);
+      PlayingCat.isCatPlaying(false,40);
     }
 }
--- src/PlayingCat.java
+++ src/PlayingCat.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class PlayingCat {
+public class PlayingCat {
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+
+        if (!summer && temperature > 24 && temperature < 36) {
+            return true;
+        } else if (summer && temperature > 24 && temperature < 46)
+            return true;
+        else
+            return false;
+
+    }
 }
